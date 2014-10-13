57d2fdb2506faba2b17b6e803fe640d72ad50cb9
Handan Kara <handan.kara@bil.omu.edu.tr>
2014-10-13 21:12:29 +0300

    Update morph.m

--- morph.m
+++ morph.m
@@ -1,7 +1,7 @@
 BW = imread('circles.png');
 imshow(BW);
 BW2 = bwmorph(BW, 'remove');     % morph iki resmin ara değerini bulup yeni resim üretmedir.
-figure
+figure                           % resimde sadece çizim görünür.
 imshow(BW2)
 BW3 = bwmorph(BW, 'skel', Inf);
 figure

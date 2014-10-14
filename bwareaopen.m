7e9f7564386b1ffe73d3272d9f89c54fbb8626e6
Handan Kara <handan.kara@bil.omu.edu.tr>
2014-10-14 20:57:17 +0300

    Update bwareaopen.m

--- bwareaopen.m
+++ bwareaopen.m
@@ -1,7 +1,7 @@
 bwOriginal = imread('text.png');                     %orjinal resimi oku.
 figure, imshow(bwOriginal)                           %orjinal resmi göster.
 
-%bwareaopen() ile orjinal resimdeki verilen pikselin altında piksele sahip olanlar kaldırılır. 
+%bwareaopen() ile orjinal resimdeki verilen pikselin altında piksel'e sahip olanlar kaldırılır. 
 %50 sayısı piksel'i gösterir değiştirilebilir.
 
 bwAreaOpen_50pixels = bwareaopen(bwOriginal, 50);   
